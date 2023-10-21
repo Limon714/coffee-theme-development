a53c26cfb5e488f8ca916daafa20e245caa80e7e
Limon Islam <72975868+Limon714@users.noreply.github.com>
2023-10-22 00:11:15 +0600

    Update functions.php

--- skillgori/functions.php
+++ skillgori/functions.php
@@ -6,5 +6,13 @@ add_theme_support( 'custom-background');
 add_theme_support( 'custom-header');
 add_theme_support( 'post-thumbnails');
 
+register_nav_menus( [
+
+      'header menu'=> 'MyiHeader Menu',
+      'footer menu'=> 'MyiFooter Menu',
+      'sidebar menu'=> 'Myisidebar Menu'
+]);
+
+
 
 ?>
